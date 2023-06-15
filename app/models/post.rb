34ee7059aedf60b103ba1c378bc304d2be392864
fuadmoin <fuadamoin@gmail.com>
2023-06-15 12:35:24 +0300

    Apply optional suggestions.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,11 +3,15 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
-  def update_post_counter
-    user.update(posts_counter: user.posts.count)
-  end
-
   def recent_posts
     comments.order(created_at: :desc).limit(5)
+  end
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    user.update(posts_counter: user.posts.count)
   end
 end
