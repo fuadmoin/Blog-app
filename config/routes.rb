9e8b38ebd8a53ba0831892a7c6a11261c9bd9ba0
fuadmoin <fuadamoin@gmail.com>
2023-06-23 19:25:02 +0300

    Add form to accept comments and posts from a current user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show create] do
+      resources :comments, only: %i[create]
+      resources :likes, only: %i[create]
+    end
+
   end
 end
