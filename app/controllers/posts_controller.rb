fa58f1c36f6e10a97e2e4a32ffe36be78e49dee8
fuadmoin <fuadamoin@gmail.com>
2023-06-24 10:33:14 +0300

    Make required changes.
    I created a new likes controller to handle post likes.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,17 +26,6 @@ class PostsController < ApplicationController
     end
   end
 
-  def like
-    @post = Post.find(params[:id])
-    @like = @post.likes.build(user: current_user)
-
-    if @like.save
-      redirect_to user_post_path(@post.user, @post)
-    else
-      render :like
-    end
-  end
-
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show create] do
-      resources :comments, only: %i[create]
-      get :like, on: :member
+    resources :posts, only: %i[index show create new] do
+      resources :comments, only: %i[create new]
+      resources :likes, only: %i[create new] do
+        collection do
+          get :create
+        end
+      end
     end
   end
 end
